c1a32c78a8541a4e09927e978b37e5b4629bc487
Justin Rodriguez <justinrodriguez@live.com>
2021-04-06 18:50:10 -0700

    Add project files.

---
+++ PCPartForum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PCPartForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
