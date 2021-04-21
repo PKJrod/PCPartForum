093014cb91b7472bae42ce78d1d53da0b54a59fd
AlgidPenguin <daniel_r129@hotmail.com>
2021-04-20 23:13:49 -0700

    Issue#18 create asp identities (#22)
    * downgraded all NuGet Packages to latest 3.1.X and added ASP Identity Pages
    
    * Renamed Database
    
    * Changed required account and modified password requirements
    
    * Modified register page and code to Create a username.
    
    * able to sign in with username
    
    Co-authored-by: Justin Rodriguez <justinrodriguez@live.com>

---
+++ PCPartForum/Models/IdentityHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PCPartForum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartForum.Models
+{
+    public static class IdentityHelper
+    {
+        public static void SetIdentityOptions(IdentityOptions options)
+        {
+            // Setting sign in options
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            // Set Password strength
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+        }
+    }
+}
