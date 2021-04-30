602c53252522e1ea6d1195ece6682dc0fea17931
Daniel Ramirez <daniel_r129@hotmail.com>
2021-04-29 19:27:54 -0700

    Updated ProductId to be primary key

--- PCPartForum/Models/Electronic.cs
+++ PCPartForum/Models/Electronic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace PCPartForum.Models
 {
     public class Electronic
     {
+        [Key]
         public int ProductId { get; set; }
 
         public string Manufacturer { get; set; }
