99ee7cc10a338dce444dfd9b4dff41ce6a70db4a
Justin Rodriguez <justinrodriguez@live.com>
2021-05-05 22:58:20 -0700

    added an admin account to

--- PCPartForum/Startup.cs
+++ PCPartForum/Startup.cs
@@ -77,6 +77,7 @@ namespace PCPartForum
                                                 , IdentityHelper.TrustedSource
                                                 , IdentityHelper.Informant
                                                 , IdentityHelper.Admin).Wait();
+            IdentityHelper.CreateDefaultAdmin(serviceProvider.ServiceProvider).Wait();
         }
     }
 }
