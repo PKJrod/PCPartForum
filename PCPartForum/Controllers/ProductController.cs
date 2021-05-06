1d662f9dc41e278570b622ca1986992c2f10e5c0
Daniel Ramirez <daniel_r129@hotmail.com>
2021-05-05 20:09:09 -0700

    Created basic controller for products with index page

---
+++ PCPartForum/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCPartForum.Controllers
+{
+    public class ProductController : Controller
+    {
+        // INDEX/INFO
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // ADD
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(IFormCollection collection)
+        {
+            return null;
+        }
+
+        // EDIT
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            return null;
+        }
+
+        // DELETE
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            return null;
+        }
+    }
+}
