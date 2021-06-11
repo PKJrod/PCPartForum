d4cb146b136e471e630ec8f31a9b62c8d2de2a2f
Justin Rodriguez <justinrodriguez@live.com>
2021-06-10 21:17:40 -0700

    Restructured migrations

---
+++ PCPartForum/Data/Migrations/20210611041403_updatedDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PCPartForum.Migrations
+{
+    public partial class updatedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
